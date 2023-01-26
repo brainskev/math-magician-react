0ad12d0a738a64efe489c3cef0aa28fe5d101981
Kelvin <ngotho.kelvin@yahoo.com>
2023-01-26 13:30:47 +0300

    Remove extra Identation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 const Footer = () => (
-    <div className="footer">
-        <div className="copyright">
-            <h4>Designed by Kelvin Kaviku</h4>
-            <p>Copyright@2022</p>
-        </div>
+  <div className="footer">
+    <div className="copyright">
+      <h4>Designed by Kelvin Kaviku</h4>
+      <p>Copyright@2022</p>
     </div>
+  </div>
 );
 
 export default Footer;
