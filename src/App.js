9a19d01da3b6f13612f798e9361f2c488d62e2c9
Kelvin <ngotho.kelvin@yahoo.com>
2023-03-22 16:59:15 +0300

    Revert "Merge pull request #2 from brainskev/components"
    This reverts commit 5a7dbbcd965be8cdb53afe7658b402068f61cb98, reversing
    changes made to 953862f8f3bf84f1b6cd50dcffe96f75c1f26e50.
    
    Unmerge for PR

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Calculator from './components/Calculator';
+import logo from './logo.svg';
+import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
